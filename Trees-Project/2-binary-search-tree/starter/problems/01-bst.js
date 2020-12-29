ad0ecc04fcc86f5f7db07d1dae17468cbc6c645c
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2020-12-29 16:57:34 -0500

    BST

--- Trees-Project/2-binary-search-tree/starter/problems/01-bst.js
+++ Trees-Project/2-binary-search-tree/starter/problems/01-bst.js
@@ -7,9 +7,53 @@ class TreeNode {
 }
 
 class BST {
+  constructor () {
+    this.root = null
+  }
 
+  insert(val) {
+    let node = new TreeNode(val)
+    if (this.root) {
+      let currentNode = this.root
+      while (currentNode !== null) {
+        if (node.val >= currentNode.val) {
+          if (!currentNode.right) {
+            currentNode.right = node
+            return this.root // returns full directory of nodes
+          } else {
+            currentNode = currentNode.right
+          }
+        } else { // node.val is less than currentNode.val
+          if (!currentNode.left) {
+            currentNode.left = node
+            return this.root
+          } else {
+            currentNode = currentNode.left
+          }
+        }
+      }
+    } else this.root = node 
+    console.log(node)
+  }
 }
+// this.root = 10
+// cn = 10
+// node - 5
+//        10
+//      5   16
+//     1 7    16
+//
+//
 
+let bst = new BST()
+bst.insert(10);
+bst.insert(5);
+bst.insert(1);
+bst.insert(16);
+bst.insert(16);
+bst.insert(7);
+
+console.log(bst)
 module.exports = {
     TreeNode, 
     BST
