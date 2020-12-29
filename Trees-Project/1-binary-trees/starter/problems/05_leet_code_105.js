267a646db74184ca6b0359de21659c506dfa67a7
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-12-29 14:37:51 -0600

    completed leetcode bonus problem

--- Trees-Project/1-binary-trees/starter/problems/05_leet_code_105.js
+++ Trees-Project/1-binary-trees/starter/problems/05_leet_code_105.js
@@ -4,5 +4,50 @@
 const { TreeNode } = require('./01_tree_node.js');
 
 function buildTree(preorder, inorder) {
+  const nodes = preorder.map(el => new TreeNode(el));
 
+  let root = nodes[0];
+
+  for (let i = 0; i < nodes.length - 2; i += 2) {
+    nodes[i].left = nodes[i + 1];
+    nodes[i].right = nodes[i + 2];
+  }
+
+  // for (let i = 0; i < nodes.length; i++) {
+  //   if (i === 0) {
+  //     root = nodes[0];
+  //   }
+
+  //   if(i === 1) {
+  //     root.left = nodes[i];
+  //   }
+
+  //   if(i ===2) {
+  //     root.right = nodes[i];
+  //   }
+
+  //   if (i === 3) {
+  //     root.right.left = nodes[i];
+  //   }
+
+  //   if (i === 4) {
+  //     root.right.right = nodes[i];
+  //   }
+
+  //   console.log('root:', root);
+  // }
+
+  return root;
 }
+
+const preorder = [3,9,20,15,7];
+const inorder = [9,3,15,20,7];
+
+console.log(buildTree(preorder, inorder));
+
+// self, left, right
+/*
+        3
+      9   20
+
+*/
