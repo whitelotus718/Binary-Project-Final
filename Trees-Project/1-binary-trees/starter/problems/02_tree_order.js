d6160ad2eb955da8f21b32e7ff6327374699e29b
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2020-12-28 19:29:15 -0500

    second

--- Trees-Project/1-binary-trees/starter/problems/02_tree_order.js
+++ Trees-Project/1-binary-trees/starter/problems/02_tree_order.js
@@ -1,9 +1,41 @@
 function preOrderArray(root) {
+  if (!root) {
+    return [];
+  }
+  let arr = [] // what is returned
+  let stack = [root] // tool
 
+while (stack.length) {
+  let ele = stack.pop()
+  arr.push(ele.val)
+  if (ele.right){
+    stack.push(ele.right)
+  }
+  if (ele.left){
+    stack.push(ele.left)
+  }
+  // arr.push(ele)
+  // arr.push(ele.right)
+  // arr.push(ele.left)
 }
 
-function inOrderArray(root) {
+return arr
+}
 
+
+// self, left, right
+//        a
+//      b   c
+//    d  e    f
+//  1  2 3 4
+        
+// left self right
+function inOrderArray(root, arr = []) {
+  if (!root) return [];
+  inOrderArray(root.left, arr)
+  arr.push(root.val)
+  inOrderArray(root.right, arr)
+  return arr
 }
 
 function postOrderArray(root) {
