97de05ca7478aabb226f03ac1556dfd32a99b994
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2020-12-29 13:08:37 -0500

    BFS

--- Trees-Project/1-binary-trees/starter/problems/04_bfs.js
+++ Trees-Project/1-binary-trees/starter/problems/04_bfs.js
@@ -1,5 +1,23 @@
 function bfs(root){
+    if (!root) return [];
+    let resultArr = []
+    let queueArr = [root]
 
+    while (queueArr.length) {
+        let el = queueArr.shift()
+        if (el.left) queueArr.push(el.left)
+        if (el.right) queueArr.push(el.right)
+        resultArr.push(el.val)
+    }
+    return resultArr;
 }
 
 module.exports = { bfs };
+
+//         a         
+//      b    c       
+//    d  e  g  f
+
+// el = 
+// resultArr = [a b c d e g f] 
+// queueArr =  []
